1b6bdd778e1aec574512be65c9ead08ab0fc4a16
jermz678 <jermz678@live.com>
2021-09-13 21:16:18 -0500

    starting shopping list app using react

---
+++ shopping-list/src/App.js
@@ -0,0 +1,12 @@
+import Textbox from './components/textbox';
+
+function App() {
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <Textbox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ shopping-list/src/components/textbox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Textbox() {
+    return (
+        <div>
+            <input type='text'/>
+        </div>
+    )
+}
+
+export default Textbox;
